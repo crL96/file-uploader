268441e63be13bd75ff3f884262a709138bb94c1
crL96 <190506813+crL96@users.noreply.github.com>
2025-05-22 15:24:07 +0200

    Refactor file upload handler to use correct placement for try catch

--- src/controllers/fileController.js
+++ src/controllers/fileController.js
@@ -14,36 +14,38 @@ const uploadPost = [
     upload.single("uploadedFile"),
 
     async (req, res) => {
-        try {
-            await cloudinary.uploader.upload_stream(
-                { // Options
-                resource_type: "raw",
-                public_id: req.file.originalname,
-                folder: "file-uploader/" + req.user.id
-                }, 
+        cloudinary.uploader
+            .upload_stream(
+                {
+                    // Options
+                    resource_type: "raw",
+                    public_id: req.file.originalname,
+                    folder: "file-uploader/" + req.user.id,
+                },
                 // Callback
                 async (error, response) => {
-                    if (error) throw error;
-
-                    await prisma.file.create({
-                        data: {
-                            name: req.file.originalname,
-                            storedName: response.display_name,
-                            path: response.url,
-                            size: req.file.size,
-                            userId: req.user.id,
-                            folderId: Number(req.params.folderId)
-                        },
-                    });
-                    console.log(response);
-                    console.log("File uploaded to cloud and added to database");
-                    res.redirect("/storage/folder/" + req.params.folderId);
-                }).end(req.file.buffer);
-
-        } catch (error) {
-            console.log(error.message);
-            res.redirect("/");
-        }
+                    try {
+                        if (error) throw error;
+                        
+                        await prisma.file.create({
+                            data: {
+                                name: req.file.originalname,
+                                storedName: response.display_name,
+                                path: response.url,
+                                size: req.file.size,
+                                userId: req.user.id,
+                                folderId: Number(req.params.folderId),
+                            },
+                        });
+                        console.log("File uploaded to cloud and added to database");
+                        res.redirect("/storage/folder/" + req.params.folderId);
+                    } catch (error) {
+                        console.log(error.message);
+                        res.redirect("/");
+                    }
+                }
+            )
+            .end(req.file.buffer);
     },
 ];
 
